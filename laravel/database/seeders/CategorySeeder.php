2bc540589ac190a0b1166a48323d70c24027a940
yume0606 <dreams1524abc@gmail.com>
2026-05-28 18:52:06 +0900

    Category,Condition,PaymentMethodシーダーを作成した

---
+++ laravel/database/seeders/CategorySeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'ファッション',
+            '家電',
+            'インテリア',
+            'レディース',
+            'メンズ',
+            'コスメ',
+            '本',
+            'ゲーム',
+            'スポーツ',
+            'キッチン',
+            'ハンドメイド',
+            'アクセサリー',
+            'おもちゃ',
+            'ベビー・キッズ',
+        ];
+        foreach ($categories as $name) {
+            Category::create(['name' => $name]);
+        }
+    }
+}
--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -18,5 +18,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            CategorySeeder::class,
+        ]);
+        $this->call([
+            ConditionSeeder::class,
+        ]);
+        $this->call([
+            PaymentMethodSeeder::class,
+        ]);
     }
 }
